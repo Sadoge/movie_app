c286a5b65359b0c4ffe020d7e5e8792542706826
Sam Smets <samsmets@hotmail.com>
2021-05-27 16:45:21 +0800

    Added navigation drawer

---
+++ lib/presentation/journeys/drawer/navigation_expanded_list_item.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/common/constants/size_constants.dart';
+import 'package:movie_app/common/extensions/size_extensions.dart';
+
+class NavigationExpandedListItem extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+  final List<String> children;
+
+  const NavigationExpandedListItem({
+    Key key,
+    @required this.title,
+    @required this.onPressed,
+    @required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).primaryColor.withOpacity(0.7),
+              blurRadius: 2,
+            ),
+          ],
+        ),
+        child: ExpansionTile(
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+          children: [
+            for (var i = 0; i < children.length; i++)
+              NavigationSubListItem(title: children[i], onPressed: () {})
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationSubListItem extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+
+  const NavigationSubListItem({
+    Key key,
+    @required this.title,
+    @required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).primaryColor.withOpacity(0.7),
+              blurRadius: 2,
+            ),
+          ],
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: Sizes.dimen_32.w),
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+      ),
+    );
+  }
+}
